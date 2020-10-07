9bd1043160b48f8fed2358694c61d5c186e96f48
LZbuilder <31080802+LZbuilder@users.noreply.github.com>
2020-10-07 13:31:09 -0500

    Update sdcardthingy.ino

--- sdcardthingy/sdcardthingy.ino
+++ sdcardthingy/sdcardthingy.ino
@@ -7,7 +7,19 @@
  */
 #include <SPI.h>
 #include <SD.h>
-
+/*
+ * https://www.arduino.cc/en/Reference/SDCardNotes
+ * Notes: 
+ *  here are the pins a nano uses digital pins 11, 12, and 13.
+ *  While the Mega uses 50, 51, and 52
+ *  Arduino Micro uses pins 14, 15, and 16.
+ *  Additionally, another pin must be used to select the SD card
+ *  You must use the 8.3 format when naming the sd card!!!
+ *    EXAMPLE "NAME001.EXT" where “NAME001” is an 
+ *    8 character or fewer string, and “EXT” is a 3 
+ *    character extension
+ *  
+ */
 
 void setup() {
   // put your setup code here, to run once:
