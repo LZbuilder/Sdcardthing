154874134792b800032d43f9fb7ab77b8ea265f8
LZbuilder <31080802+LZbuilder@users.noreply.github.com>
2020-10-07 12:54:35 -0500

    Made comments

--- sdcardthingy/sdcardthingy.ino
+++ sdcardthingy/sdcardthingy.ino
@@ -1,3 +1,14 @@
+/*
+ * Now I must make an
+ * annoying ass piece of
+ * code for my friend
+ * 
+ * to begin I need to look
+ */
+#include <SPI.h>
+#include <SD.h>
+
+
 void setup() {
   // put your setup code here, to run once:
 
