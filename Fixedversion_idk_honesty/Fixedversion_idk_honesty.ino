39d08b476a1d5325953cbcc6b8023aa36ca467b3
LZbuilder <31080802+LZbuilder@users.noreply.github.com>
2020-10-17 10:57:24 -0500

    Update Fixedversion_idk_honesty.ino

--- Fixedversion_idk_honesty/Fixedversion_idk_honesty.ino
+++ Fixedversion_idk_honesty/Fixedversion_idk_honesty.ino
@@ -1,82 +1,89 @@
+#include <SPI.h>
+#include <SD.h>
+File myFile;
+
+
+
+//work in progres
 String strstuff = "G23 X42 Y54.245   Z4; E31; ";
-String G;
-double x; double y; //end result of what comes after x
-String xval = ""; String yval = ""; String zval = "";//for computing stuff and combing numbers :)
-int i; //simple integer used for what specific character in the string we are using!
-int lengthstr; // The length of the string entered!
+String g;
+String gval;
+String xval = "";
+String yval = "";
+double x; //end result of what comes after x
+double y; //end result of what comes after y
+int i;
+int lengthstr;
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
-  while (!Serial) {
-
+  while (!Serial){
+    
   }
-  Serial.println("Code is starting!");
-
-
-
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  //open file for reading
+  if (myFile) {
+    Serial.println("gcode.txt:");
+ 
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening gcode.txt");
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println(strstuff[0]);
-  if (Serial.available()) {
+  if (Serial.available()){
     //now you run code after you've entered that dumb string.
     lengthstr = strstuff.length();
     i = 0;
     x = 0;
     y = 0;
-
-    while (i < lengthstr) {
-      if (strstuff[i] == 'G') {
-        //the character is G
-
-        if (isDigit(strstuff[i + 1])) {
-          //There is a digit after G
-          
-
-        }
-        else {
+    
+    while (i<=lengthstr){
+      if (strstuff[i] == 'G'){
+        
+        if (isDigit(strstuff[i + 1])){
           i++;
-        }
-
-      }
-      else if (strstuff[i] == 'X') {
-        if (isDigit(strstuff[i + 1])) {
-          i++;
-          while (isDigit(strstuff[i]) || strstuff == '.') {
-            xval += strstuff[i];
-            i++;
-          }
-
-        }
-      }
-
-      else if (strstuff[i] == 'Y') {
-        if (isDigit(strstuff[i + 1])) {
-          i++;
-          while (isDigit(strstuff[i]) || strstuff == '.') {
-            yval += strstuff[i];
+          while (isDigit(i)) {
+            
             i++;
           }
         }
-
-        else if (strstuff[i] == 'Z') {
-          if (isDigit(strstuff[i + 1])) {
-            i++;
-            while (isDigit(strstuff[i]) || strstuff == '.') {
-              zval += strstuff[i];
-              i++;
-            }
+        else{
+          i++;
+        }
+        
+      }
+      else if (strstuff[i] == 'X'){
+        if (isDigit(strstuff[i + 1])){
+          //the character after X is a digit
+          i++;
+          while (isDigit(i)){
+            
           }
         }
-
-
-
       }
-
-
+      else if (strstuff[i] == 'Y'){
+        
+      }
+      
+      
+      
     }
   }
+  
+
 }
