01c57ff1d3fd6207df818cd6e7f56211d358182b
LZbuilder <31080802+LZbuilder@users.noreply.github.com>
2020-10-18 18:11:21 -0500

    Big This

--- Fixedversion_idk_honesty/Fixedversion_idk_honesty.ino
+++ Fixedversion_idk_honesty/Fixedversion_idk_honesty.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <SD.h>
+
 File myFile;
 
 
@@ -7,11 +8,13 @@ File myFile;
 //work in progres
 String strstuff = "G23 X42 Y54.245   Z4; E31; ";
 String g;
-String gval;
+String gval = "";
 String xval = "";
 String yval = "";
 double x; //end result of what comes after x
 double y; //end result of what comes after y
+char singleletterchar; int singleletterint;
+String multiple[] = {};
 int i;
 int lengthstr;
 
@@ -19,71 +22,40 @@ void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
-  while (!Serial){
-    
+  while (!Serial) {
+    ; //Wait for serial monitor to be opened
   }
+
+  Serial.print("Initializing SD card...");
+
   if (!SD.begin(4)) {
     Serial.println("initialization failed!");
     while (1);
   }
+
+  Serial.println("initialization done.");
+
   //open file for reading
+  myFile = SD.open("gcode.txt");
+
   if (myFile) {
     Serial.println("gcode.txt:");
- 
+
     // read from the file until there's nothing else in it:
     while (myFile.available()) {
-      Serial.write(myFile.read());
+
+      singleletterint = myFile.read();
+      singleletterchar = char(singleletterint);
+      
+
     }
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening gcode.txt");
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(strstuff[0]);
-  if (Serial.available()){
-    //now you run code after you've entered that dumb string.
-    lengthstr = strstuff.length();
-    i = 0;
-    x = 0;
-    y = 0;
-    
-    while (i<=lengthstr){
-      if (strstuff[i] == 'G'){
-        
-        if (isDigit(strstuff[i + 1])){
-          i++;
-          while (isDigit(i)) {
-            
-            i++;
-          }
-        }
-        else{
-          i++;
-        }
-        
-      }
-      else if (strstuff[i] == 'X'){
-        if (isDigit(strstuff[i + 1])){
-          //the character after X is a digit
-          i++;
-          while (isDigit(i)){
-            
-          }
-        }
-      }
-      else if (strstuff[i] == 'Y'){
-        
-      }
-      
-      
-      
-    }
-  }
-  
+
+  //nothing!
+
 
 }
