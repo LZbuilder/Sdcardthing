4906263e3b0c4105474022e88d0ae5ce0c9fcaf2
LZbuilder <31080802+LZbuilder@users.noreply.github.com>
2020-11-02 10:21:07 -0600

    Update alexanders_code.ino

--- alexanders_code/alexanders_code.ino
+++ alexanders_code/alexanders_code.ino
@@ -1,11 +1,11 @@
 
 #include <Stepper.h>
-#include <Servo.h> 
+#include <Servo.h>
 Servo servoy;
 
 int Bedheight = 60;//the height of the centerpoint of the rotating on the Y servo from the print bed
 double xValue = 20;// the numbers followed after G1  (example G1 X___ Y)
-double yValue = 20;// the numbers followed after G1 X (example G1 X Y___)   
+double yValue = 20;// the numbers followed after G1 X (example G1 X Y___)
 int time = 1;
 
 //  ↑ varibles used by evreything
@@ -23,21 +23,21 @@ double servoNewsdeg = 0 ;// the new degree for the servo,formaly called yD
 double servoCalculateddeg = 0;
 double servoPreviousdeg = 0;
 double servoCurrentpos = 0;
-//  ↑ varibles used by the spinnig servomotor 
+//  ↑ varibles used by the spinnig servomotor
 
 
 Stepper Stepper1(stepsPerRevolution, 7, 5, 6, 8);
 
 void setup() {
- 
-  
-Serial.begin(9600);
 
-Stepper1.setSpeed(5);
-servoy.attach(9);
 
-servoy.write(0);
-servoCurrentpos = 0;
+  Serial.begin(9600);
+
+  Stepper1.setSpeed(5); //rpms
+  servoy.attach(9);
+
+  servoy.write(0);
+  servoCurrentpos = 0;
 }
 
 
@@ -45,28 +45,28 @@ servoCurrentpos = 0;
 
 
 void loop() {
-  double stepperNewdeg = ((atan(xValue/yValue)*180/3.14159265)+0);// caclulate how much the stepper neews t move
-  double servoNewdeg = (atan(Bedheight / yValue)*180 / 3.14159265); // caclulate how much the servo needs to move
-  stepperCalculateddeg = stepperNewdeg-stepperPreviousdeg; // how far the stepper needs to move in degrees
+  double stepperNewdeg = ((atan(xValue / yValue) * 180 / 3.14159265) + 0); // caclulate how much the stepper neews t move
+  double servoNewdeg = (atan(Bedheight / yValue) * 180 / 3.14159265); // caclulate how much the servo needs to move
+  stepperCalculateddeg = stepperNewdeg - stepperPreviousdeg; // how far the stepper needs to move in degrees
   servoCalculateddeg = servoNewdeg - servoPreviousdeg;
-  Serial.println(stepperNewdeg-stepperPreviousdeg);
-   
-   // their are four possibilitys,
-   //the stepper rotates clockwise and the servo degree of the servo decrespperCalculateddeg<0 & servoCalculateddeg<servoPreviousdeg
-   //the stepper rotates clockwise and the degree of the servo increses,                       stepperCalculateddeg<0 & servoCalculateddeg>servoPreviousdeg
-   //the stepper rotates counterclockwise and the degree of the servo increses         stepperCalculateddeg<0 & servoCalculateddeg<servoPreviousdeg
-   //the stepper rotates counterclockwise and the degree of the servo decreses  
-    
-  if (stepperCalculateddeg>0 && servoCalculateddeg>servoPreviousdeg);{ // the calculated value for the stepper is more than 0 go forwards,the calculated value for the stepper is more than the last value make the angle more
-  Serial.println("stepper moving counter clock wise");
-   Serial.println("degree of the servo should eb getting higher");
-  // speed = distance/time 
-  // do the formula so the stepper arrives to stepperCalculateddeg the same time the servo arrives to servoCalculateddeg
-    
+  Serial.println(stepperNewdeg - stepperPreviousdeg);
+
+  // their are four possibilitys,
+  //the stepper rotates clockwise and the servo degree of the servo decrespperCalculateddeg<0 & servoCalculateddeg<servoPreviousdeg
+  //the stepper rotates clockwise and the degree of the servo increses,                       stepperCalculateddeg<0 & servoCalculateddeg>servoPreviousdeg
+  //the stepper rotates counterclockwise and the degree of the servo increses         stepperCalculateddeg<0 & servoCalculateddeg<servoPreviousdeg
+  //the stepper rotates counterclockwise and the degree of the servo decreses
+
+  if (stepperCalculateddeg > 0 && servoCalculateddeg > servoPreviousdeg); { // the calculated value for the stepper is more than 0 go forwards,the calculated value for the stepper is more than the last value make the angle more
+    Serial.println("stepper moving counter clock wise");
+    Serial.println("degree of the servo should eb getting higher");
+    // speed = distance/time
+    // do the formula so the stepper arrives to stepperCalculateddeg the same time the servo arrives to servoCalculateddeg
+
   }
-//  servoy.write(yD);
-  xValue = random(0,200);
-  yValue = random(0,200);
- stepperPreviousdeg = stepperNewdeg; // sets the steppers previous degre to the last degre used
- servoPreviousdeg = servoNewdeg; // does the same but for the servo
+  //  servoy.write(yD);
+  xValue = random(0, 200);
+  yValue = random(0, 200);
+  stepperPreviousdeg = stepperNewdeg; // sets the steppers previous degre to the last degre used
+  servoPreviousdeg = servoNewdeg; // does the same but for the servo
 }
