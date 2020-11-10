3b5bab54900d82a2464a9e42ba2f68aa37cb199e
LZbuilder <31080802+LZbuilder@users.noreply.github.com>
2020-11-10 13:09:38 -0600

    Thing

--- SlaveReciever/SlaveReciever.ino
+++ SlaveReciever/SlaveReciever.ino
@@ -1,49 +1,43 @@
 #include <Wire.h>
 #include <Stepper.h>
-#include <Servo.h> 
-Servo servoy;
+#include <Servo.h>
+
 boolean lukaval = true;
-int Bedheight = 60;//the height of the centerpoint of the rotating on the Y servo from the print bed
-double xValue = 20;// the numbers followed after G1  (example G1 X___ Y)
-double yValue = 20;// the numbers followed after G1 X (example G1 X Y___)   
-int time = 1;
 
-//  ↑ varibles used by evreything
-
+Servo servoy;
 int stepsPerRevolution = 2038;
-
-double stepperNewdeg = 0;
-double stepperCalculateddeg = 0;
-double stepperPreviousdeg = 0;
-double stepperCurrentpos = 0;
-
-//  ↑ varibles used by the stepper motors
-
-double servoNewsdeg = 0 ;// the new degree for the servo,formaly called yD
-double servoCalculateddeg = 0;
-double servoPreviousdeg = 0;
-double servoCurrentpos = 0;
-//  ↑ varibles used by the spinnig servomotor 
-
-
+int Bedheight = 60;//the height of the centerpoint of the rotating on the Y servo from the print bed
+double xValue = 20;// the numbers followed after G1  (example G1 X Y)
+double yValue = 20;// the numbers followed after G1 X (example G1 X Y)
+double rawdeg = 0;
+double calculateddeg = 0;
+double previousdeg = 0;
+double degdiff = 0;  //degree diffrence,useful for calculating if we need to move slow or fast,also the ablsoute value of calculateddeg
 Stepper Stepper1(stepsPerRevolution, 7, 5, 6, 8);
-
 void setup() {
- 
-  
-Serial.begin(9600);
+  Stepper1.step(0);
+  // put your setup code here, to run once:
+  Serial.begin(9600);
 
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 Stepper1.setSpeed(5);
 servoy.attach(9);
 
 servoy.write(0);
 servoCurrentpos = 0;
+=======
+  Stepper1.setSpeed(5);
+  servoy.attach(9);
+  servoy.write(90);
+>>>>>>> parent of 8f49a83... Update SlaveReciever.ino
   Wire.begin(4);                // join i2c bus with address #4
   Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);
 }
 
 void loop() {
+<<<<<<< HEAD
   double stepperNewdeg = ((atan(xValue/yValue)*180/3.14159265)+0);// caclulate how much the stepper neews t move
   double servoNewdeg = (atan(Bedheight / yValue)*180 / 3.14159265); // caclulate how much the servo needs to move
   stepperCalculateddeg = ((((stepperNewdeg-stepperPreviousdeg)/5.661111)*32)*3); // how far the stepper needs to move in degrees
@@ -63,6 +57,12 @@ void loop() {
   calculateddeg = rawdeg - previousdeg;
   degdiff = abs(calculateddeg);
   //Stepper1.setSpeed(pow((degdiff / ), 1.00001));
+=======
+  double rawdeg = ((atan(xValue / yValue) * 180 / 3.14159265) + 0);
+  calculateddeg = rawdeg - previousdeg;
+  degdiff = abs(calculateddeg);
+  Stepper1.setSpeed(pow((degdiff / ), 1.00001));
+>>>>>>> parent of 8f49a83... Update SlaveReciever.ino
   Stepper1.step(((calculateddeg / 5.661111) * 32) * 3);
 
   double yD = (atan(Bedheight / yValue) * 180 / 3.14159265);
@@ -70,6 +70,7 @@ void loop() {
   //xValue = random(0,200);
   //yValue = random(0,200);
   previousdeg = calculateddeg;
+<<<<<<< HEAD
 }
 
 void receiveEvent() {
@@ -96,16 +97,19 @@ void receiveEvent() {
   yValue = random(0,200);
  stepperPreviousdeg = stepperNewdeg; // sets the steppers previous degre to the last degre used
  servoPreviousdeg = servoNewdeg; // does the same but for the servo
+=======
+>>>>>>> parent of 8f49a83... Update SlaveReciever.ino
 }
+
 void receiveEvent() {
   if (lukaval) {
-    String lukastring = String(Wire.read());
+    String lukastring = Wire.read();
     lukastring += Wire.read();
     xValue = lukastring.toDouble();
     lukaval = false;
   }
   if (!lukaval) {
-    String lukastring = String(Wire.read());
+    String lukastring = Wire.read();
     lukastring += Wire.read();
     yValue = lukastring.toDouble();
     lukaval = true;
