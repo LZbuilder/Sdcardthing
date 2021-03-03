746056c0cc1d6edce1193953536a32a38899cab2
LZbuilder <31080802+LZbuilder@users.noreply.github.com>
2021-03-02 21:45:08 -0600

    Update listfiles.ino

--- listfiles/listfiles.ino
+++ listfiles/listfiles.ino
@@ -1,21 +1,21 @@
- /*
+/*
   Listfiles
 
   This example shows how print out the files in a
   directory on a SD card
 
   The circuit:
-   SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
+  SD card attached to SPI bus as follows:
+** MOSI - pin 11
+** MISO - pin 12
+** CLK - pin 13
+** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
 
   created   Nov 2010
   by David A. Mellis
   modified 9 Apr 2012
   by Tom Igoe
-  modified 2 Feb 2014
+  modified 2 Feb  2014
   by Scott Fitzgerald
 
   This example code is in the public domain.
@@ -40,11 +40,11 @@ void setup() {
     while (1);
   }
   Serial.println("initialization done.");
-
+  delay(1);
   root = SD.open("/");
-
+  delay(1);
   printDirectory(root);
-
+  delay(1);
 }
 
 void loop() {
@@ -60,11 +60,11 @@ void printDirectory(File dir) {
       // no more files
       break;
     }
-    
-  if (i != 0){
+
+    if (i != 0) {
       Serial.println(entry.name());
-  }
-    
+    }
+
     entry.close();
     i++;
   }
